ab84f8e30dcdcd739e406ffe7c16c9c8399d0f84
luisxde <diazcaleroluismario@gmail.com>
2024-06-11 21:34:43 -0500

    EDITAR TODO

--- obj/bd.php
+++ obj/bd.php
@@ -2,8 +2,8 @@
 // Incluye el archivo de configuración
 include 'config.php';
 
-// Función para establecer la conexión a la base de datos
-function conectar() {
+// Función para conectar a la base de datos
+function conectar(){
     // Crear una conexión a la base de datos
     $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
@@ -16,33 +16,38 @@ function conectar() {
 }
 
 // Función de inicio de sesión
-function login($usuario, $pass) {
-    // Conectar a la base de datos
+function login($usuario, $pass){
     $conexion = conectar();
-
-    // Preparar la consulta SQL
     $sql = $conexion->prepare("SELECT id_persona, password FROM persona WHERE correo = ?");
+    if (!$sql) {
+        die("Error en la preparación de la consulta: " . $conexion->error);
+    }
+    
     $sql->bind_param("s", $usuario);
     $sql->execute();
     $sql->store_result();
 
-    // Verificar si el usuario existe
     if ($sql->num_rows > 0) {
-        // Vincular resultados
         $sql->bind_result($id, $hashed_password);
-        $sql->fetch(); // Obtener los resultados
+        $sql->fetch(); // Obtener resultados
 
-        // Verificar la contraseña
         if (password_verify($pass, $hashed_password)) {
-            // Iniciar sesión y almacenar el ID del usuario en la sesión
             session_start();
             $_SESSION['id'] = $id;
-            return 1; // Inicio de sesión exitoso
-        } else {
-            return 0; // Contraseña incorrecta
+            header('Location: menu.php');
+            exit();
         }
-    } else {
-        return 0; // Usuario no encontrado
+    }
+    header('Location: error.php');
+    exit();
+}
+
+// Redirección si el usuario está logueado
+function verificar_sesion() {
+    session_start();
+    if (!isset($_SESSION['id'])) {
+        header('Location: index.php');
+        exit();
     }
 }
 ?>
