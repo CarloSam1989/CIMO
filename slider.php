a41b96287cda30f972b778501cc7e0f85a7a957c
luisxde <diazcaleroluismario@gmail.com>
2024-07-09 18:56:47 -0500

    editar slider

--- slider.php
+++ slider.php
@@ -19,41 +19,66 @@
 </head>
 <body>
     <header>
-        <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
-            <div class="container-fluid">
-                <img src="img/logocimo.ico" alt="Logo" class="navbar-logo">
-                <a class="navbar-brand" href="form.html">CIMO</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+    <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
+    <div class="container-fluid">
+        <img src="img/logocimo.ico" alt="Logo" class="navbar-logo">
+        <a class="navbar-brand" href="#">CIMO</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <?php include_once('obj/bd.php'); ?>
+        <div class="collapse navbar-collapse" id="navbarColor01">
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="bolsa.php">Aplicar Empleo</a>
+                </li>
+                <?php if (verificar_sesion()): ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="crear_Noticias.php">Crear Noticia</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="crear_Empleo.php">Crear Empleo</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="mostrar_Bacantes.php">Mostrar Bacantes</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="slider.php">Slider</a>
+                    </li>
+                <?php else: ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="login.html">Login</a>
+                    </li>
+                <?php endif; ?>
+            </ul>
+            <form class="d-flex">
+                <button class="boton-social" id="social"> 
+                    <span class="icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="33" viewBox="0 0 512 512" height="33">
+                            <g fill-rule="evenodd" clip-rule="evenodd">
+                                <path fill="#3a5ba2" d="m256.23 512c140.58 0 255.77-115.19 255.77-255.77 0-141.046-115.19-256.23-255.77-256.23-141.046 0-256.23 115.184-256.23 256.23 0 140.58 115.184 255.77 256.23 255.77z"></path>
+                                <path fill="#fff" d="m224.023 160.085c0-35.372 28.575-63.946 63.938-63.946h48.072v63.946h-32.199c-8.608 0-15.873 7.257-15.873 15.873v32.192h48.072v63.938h-48.072v144.22h-63.938v-144.22h-48.065v-63.938h48.065z"></path>
+                            </g>
+                        </svg>
+                    </span>
+                    <span class="text1 text-dark">Síguenos</span>
+                    <span class="text2">1,2k</span> 
                 </button>
-                <div class="collapse navbar-collapse" id="navbarColor01">
-                    <ul class="navbar-nav me-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Noticias</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Contactos</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Acerca de</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Slaider</a>
-                        </li>                               
-                    </ul>
-                    <form class="d-flex">
-                        <button class="boton-social" id="social"> 
-                          <span class="icon"><svg xmlns="http://www.w3.org/2000/svg" width="33" viewBox="0 0 512 512" height="33"><g fill-rule="evenodd" clip-rule="evenodd"><path fill="#3a5ba2" d="m256.23 512c140.58 0 255.77-115.19 255.77-255.77 0-141.046-115.19-256.23-255.77-256.23-141.046 0-256.23 115.184-256.23 256.23 0 140.58 115.184 255.77 256.23 255.77z"></path><path fill="#fff" d="m224.023 160.085c0-35.372 28.575-63.946 63.938-63.946h48.072v63.946h-32.199c-8.608 0-15.873 7.257-15.873 15.873v32.192h48.072v63.938h-48.072v144.22h-63.938v-144.22h-48.065v-63.938h48.065z"></path></g></svg></span>
-                          <span class="text1 text-dark">Síguenos</span>
-                          <span class="text2">1,2k</span> 
+                <?php if (verificar_sesion()): ?>
+                    <div class="dropdown">
+                        <button class="btn btn-link dropdown-toggle text-light" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                            <i class="fas fa-user"></i>
                         </button>
-                        <a href="logout.php" class="btn-login"><i class="fas fa-sign-out-alt"></i> Cerrar Sesión</a>                       
-                    </form>
-                </div>
-            </div>
-        </nav>
-    </header>
-
+                        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton1">
+                            <li><a class="dropdown-item" href="logout.php">Cerrar sesión</a></li>
+                        </ul>
+                    </div>
+                <?php endif; ?>
+            </form>
+        </div>
+    </div>
+</nav>
+<header>
     <main class="container">
         <h1>Administrar Carrusel</h1>
         
